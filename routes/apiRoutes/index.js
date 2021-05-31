53a36fbfc862ea76baae23ac493fd698d4fe4c34
yana-mkr <yana.mkrtchyan@gmail.com>
2021-05-31 13:52:40 -0600

    clean up

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,9 @@
+// pulls all the routes together
+
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+router.use(require('./partyRoutes'));
+
+module.exports = router;
